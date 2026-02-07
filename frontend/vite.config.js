401abf16150d3d6f5b96870ee8d775d396b70d51
Gasim Jabrayilov <jabra@Gasims-MacBook-Air.local>
2026-02-07 17:13:55 +0400

    config file adjusted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig((command, mode) => {
-  const env = loadEnv(mode, process.cwed(), "")
+  const env = loadEnv(mode, process.cwd(), "")
 
   console.log(env.VITE_DEBUG)
 
